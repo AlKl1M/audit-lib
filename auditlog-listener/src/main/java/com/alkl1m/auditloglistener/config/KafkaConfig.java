fc46b653697d2ae31028aceb17e4d3f3a8b0e51a
AlKl1M <al.klimkovetskiy@gmail.com>
2024-08-06 20:39:28 +0200

    refactor: параметры конфига теперь в свойствах и используются через @Value

--- auditlog-listener/src/main/java/com/alkl1m/auditloglistener/config/KafkaConfig.java
+++ auditlog-listener/src/main/java/com/alkl1m/auditloglistener/config/KafkaConfig.java
@@ -4,6 +4,7 @@ package com.alkl1m.auditloglistener.config;
 import com.alkl1m.auditloglistener.payload.AuditLogEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -19,14 +20,29 @@ import java.util.Map;
 @EnableKafka
 public class KafkaConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServer;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+
+    @Value("${spring.kafka.consumer.auto-offset-reset}")
+    private String autoOffsetReset;
+
+    @Value("${spring.kafka.consumer.enable-auto-commit}")
+    private String enableAutoCommit;
+
+    @Value("${spring.kafka.consumer.isolation-level}")
+    private String isolationLevel;
+
     @Bean
     public ConsumerFactory<String, AuditLogEvent> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group-1");
-        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
-        config.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
+        config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, enableAutoCommit);
+        config.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, isolationLevel);
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), new JsonDeserializer<>(AuditLogEvent.class));
