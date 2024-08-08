dc4148b3ce5c80fcac17ba7b6a8ebb6bcd9b0dd6
AlKl1M <al.klimkovetskiy@gmail.com>
2024-08-08 16:33:56 +0200

    refactor: поменял геттеры и сеттеры на ломбок

--- auditlog-spring-boot-autoconfigure/src/main/java/com/alkl1m/auditlogspringbootautoconfigure/domain/AuditLogEntry.java
+++ auditlog-spring-boot-autoconfigure/src/main/java/com/alkl1m/auditlogspringbootautoconfigure/domain/AuditLogEntry.java
@@ -1,43 +1,18 @@
 package com.alkl1m.auditlogspringbootautoconfigure.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuditLogEntry implements Serializable {
     private String serverSource;
     private String method;
     private Object[] args;
     private Object result;
     private String exception;
-
-    public AuditLogEntry(String serverSource, String method, Object[] args, Object result, String exception) {
-        this.serverSource = serverSource;
-        this.method = method;
-        this.args = args;
-        this.result = result;
-        this.exception = exception;
-    }
-
-    public String getMethod() {
-        return method;
-    }
-
-    public Object[] getArgs() {
-        return args;
-    }
-
-    public Object getResult() {
-        return result;
-    }
-
-    public String getException() {
-        return exception;
-    }
-
-    public String getServerSource() {
-        return serverSource;
-    }
-
-    public void setServerSource(String serverSource) {
-        this.serverSource = serverSource;
-    }
 }
